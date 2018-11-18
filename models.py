0321196098da1ab2804db86c61c6392a30b7edcf
Abhijeet Mulgund <abhmul@gmail.com>
2018-11-18 17:59:45 -0600

    Updated nbsvm model

--- models.py
+++ models.py
@@ -1,33 +1,32 @@
 """
 Implement a Naive Bayes SVM!
 """
-
+import numpy as np
+from scipy import sparse
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
-from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
 
 class NbSvmClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self,
-                 clf = LinearSVC(),
-                 clf__C = 1.0,
-                 clf__dual = 'auto',
-                 clf__verbose=0):
-
-        set_params(
-            clf = clf,
-            clf__C = clf__C,
-            clf__dual = clf__dual,
-            clf__verbose, clf__verbose
-        )
+    def __init__(self, C=1.0, dual='auto', verbose=0):
+        self.C = C
+        self.dual = dual
+        self.verbose = verbose
+        self._clf = None
+        print("Creating model with C=%s" % C)
 
     def predict(self, x):
         # Verify that model has been fit
-        check_is_fitted(self, ['r_'])
-        return self._clf.predict(x.multiply(self.r_))
+        check_is_fitted(self, ['_r', '_clf'])
+        return self._clf.predict(x.multiply(self._r))
 
     def score(self, x, y):
-        check_is_fitted(self, ['r_'])
-        return self._clf.score(x.multiply(self.r_), y)
+        check_is_fitted(self, ['_r', '_clf'])
+        return self._clf.score(x.multiply(self._r), y)
+
+    def predict_proba(self, x):
+        check_is_fitted(self, ['_r', '_clf'])
+        return self._clf.predict_proba(x.multiply(self._r))
 
     def fit(self, x, y):
         # Check that X and y have correct shape
@@ -37,10 +36,10 @@ class NbSvmClassifier(BaseEstimator, ClassifierMixin):
             p = x[y == y_i].sum(0)
             return (p + 1) / ((y == y_i).sum() + 1)
 
-        self.r_ = sparse.csr_matrix(np.log(pr(x, 1, y) / pr(x, 0, y)))
-        x_nb = x.multiply(self.r_)
+        self._r = sparse.csr_matrix(np.log(pr(x, 1, y) / pr(x, 0, y)))
+        x_nb = x.multiply(self._r)
         if self.dual == 'auto':
             self.dual = x_nb.shape[0] <= x_nb.shape[1]
-        self._clf = LinearSVC(C=self.C, dual=self.dual, verbose=self.verbose)
+        self._clf = LogisticRegression(C=self.C, dual=self.dual, verbose=self.verbose)
         self._clf.fit(x_nb, y)
         return self
