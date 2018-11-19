48c7f39ed83e7893e6d881c71ed33718ad57b632
Abhijeet Mulgund <abhmul@gmail.com>
2018-11-18 18:02:42 -0600

    added data loading

--- get-data.sh
+++ get-data.sh
@@ -3,6 +3,7 @@
 mkdir -p data && \
     pushd data && \
     curl -O http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz && \
+    curl -O https://github.com/abhmul/DataScienceTrack/raw/master/NLP/imdb_dataset.zip && \
     echo "Extracting data..." && \
     tar zxf aclImdb_v1.tar.gz && \
     echo "Done!" && \
